d134bedf1f2fdac1c1947ef9c988337f85433594
Thomas TIENNOT <TIENNOT@MacBook-Pro-de-Thomas.local>
2018-10-14 09:54:05 +0100

    Change of picture

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,34 @@
+
+//Changing the picture by clicking on it
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/picture.jpg') {
+      myImage.setAttribute ('src','images/picture_2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/picture.jpg');
+    }
+}
+
+//Creation of button for user selection
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome on my webpage, ' + storedName + '!' ;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome on my webpage, ' + storedName + '!' ;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
